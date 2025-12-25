b783736fe551dd4895d6be5f041d7e8d6e5fb495
CarloDM <carlo.de.mauro.work@gmail.com>
2025-12-25 12:24:18 +0100

    vector , push

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ impl Person {
 
 fn main() {
 
+    // STRUCT
     // crea persona diretamente, cambia nome con funzione implementata in struttura
     let mut persona_due = Person {
         nome : String::from("Mark"), 
@@ -34,6 +35,22 @@ fn main() {
 
     println!("nome: {}, cognome: {}, eta {}.", persona_uno.nome, persona_uno.cognome, persona_uno.eta);
 
+
+    // VECTOR
+    // standar con questa syntassi ha il tipo fisso puo contenerte solo uyn tipo di dato
+    // sara possibile in rust avere anche il tipo dinamico ma non ora.
+    let mut mio_vettore: Vec<u32> = Vec::new();
+    println!("{:?}", mio_vettore);
+    mio_vettore.push(33);
+    mio_vettore.push(43);
+    mio_vettore.push(77);
+
+    for n in mio_vettore.iter(){
+        print!("{}, ", n);
+    }
+
+    let num_di_vettore = mio_vettore[1];
+    println!("secondo numero del vettore {}", num_di_vettore)
 
 
 }
